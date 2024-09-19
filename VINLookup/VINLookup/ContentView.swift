07ac453d30876d6671c8dd9bb5c611aef6b008e3
Krzysztof Lech <krzysztof.lech@24abc.pl>
2024-09-19 13:43:15 +0200

    Xcode project files added

---
+++ VINLookup/VINLookup/ContentView.swift
@@ -0,0 +1,20 @@
+//  ContentView.swift
+//  Created by Krzysztof Lech on 19/09/2024.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VINLookup/VINLookup/VINLookupApp.swift
@@ -0,0 +1,13 @@
+//  VINLookupApp.swift
+//  Created by Krzysztof Lech on 19/09/2024.
+
+import SwiftUI
+
+@main
+struct VINLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
